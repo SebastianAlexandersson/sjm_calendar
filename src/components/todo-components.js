c839543e6190d648ee44f6606e40fe9521f6e976
Joachim Ekström <joachim.ekstroem@gmail.com>
2019-10-15 11:26:19 +0200

    Starting to add code for Todo-handeling

--- src/components/todo-components.js
+++ src/components/todo-components.js
@@ -1,4 +1,6 @@
+// Generate button for adding a Todo
 Vue.component('add-todo', {
+
   data() {
     return {
       counter: 0
@@ -9,10 +11,45 @@ Vue.component('add-todo', {
 
     }
   },
-  template: '<input v-on:click="onClick" type="button" value="Add new Todo">'
+  template: `<div>
+                <input v-on:click="onClick" type="button" value="Add new Todo">
+            </div>`
+})
+// When add-todo button is pressed, open this modal for adding data.
+Vue.component('add-todo-modal', {
+  created() {
+    this.eventId = "newid" // Uuid
+    this.eventDedline = "now + 1day?" // Use moment to fix this.
+
+  },
+  data() {
+    return {
+      newTodo: {
+        eventId, // a random generated Id:number ---> 
+        eventGroup, // Group color i.e. Red,Green.....
+        eventType, // Todo or Meeting
+        eventStartTime, // Start time of the Meeting
+        eventStopTime, // Stop time of the Meeting
+        eventDedline, // Deadline for Todo
+        eventHeadline, // Event headline
+        eventText, // Event text for body
+      }
+    }
+  },
+  methods: {
+    onClick() {
+
+    }
+  },
+  template: `<div>
+                
+            </div>`
 })
 
+
+// Button for modify Todo. One per todo.
 Vue.component('modify-todo', {
+  props: ['todoEvent'],
   data() {
     return {
       counter: 0
@@ -20,13 +57,32 @@ Vue.component('modify-todo', {
   },
   methods: {
     onClick() {
-
+      // Open modal and use "todoEvent" as payload
     }
   },
   template: '<input v-on:click="onClick" type="button" value="Edit">'
 })
+// When modify-todo button is pressed, open this modal for modifing data.
+Vue.component('modify-todo-modal', {
+  props: ['todoEvent'],
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    onClick() {
 
+    }
+  },
+  template: `<div>
+                
+            </div>`
+})
+
+// Button for delete the todo. Opens an modal to ask if sure to delete.
 Vue.component('delete-todo', {
+  props: ['todoEvent'],
   data() {
     return {
       counter: 0
@@ -38,4 +94,21 @@ Vue.component('delete-todo', {
     }
   },
   template: '<input v-on:click="onClick" type="button" value="Delete">'
+})
+// When delete-todo button is pressed, open this modal for deletion of data.
+Vue.component('delete-todo-modal', {
+  props: ['todoEvent'],
+  data() {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    onClick() {
+      // delete the event with this ID in store object.
+    }
+  },
+  template: `<div>
+                
+            </div>`
 })
--- src/store.js
+++ src/store.js
@@ -3,22 +3,24 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+
+
+
 export default new Vuex.Store({
   state: {
-    eventObject: { // Master object for all types of event i.e. Meetings and Todos.
-      eventId, // a random generated Id:number ---> 
-      eventGroup, // Group color i.e. Red,Green.....
-      eventType, // Todo or Meeting
-      eventStartTime, // Start time of the Meeting
-      eventStopTime, // Stop time of the Meeting
-      eventDedline, // Deadline for Todo
-      eventHeadline, // Event headline
-      eventText, // Event text for body
-
-
-    }
+    events: {},
   },
   mutations: {
+    addTodo(newTodo) {
+      events.push(newTodo)
+    },
+    modifyTodo(modifiedTodo) {
+      // Find id of todo and replace with argumented todo
+    },
+    deleteTodo(deleteTodo) {
+      // find id of todo and delete this object.
+    }
+
 
   },
   actions: {
