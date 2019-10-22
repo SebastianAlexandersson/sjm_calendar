6a71b84c2d00c3a929be35817a798a993851b155
Sebastian Alexandersson <sebbe@sebbe.dev>
2019-10-22 14:26:15 +0200

    asdf

--- src/components/Calendar/Weeks.vue
+++ src/components/Calendar/Weeks.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="weeks">
-    <div class="week" v-for='week in get.numWeeks' :key='`${week}week`'>
+    <div class="week" v-for="week in get.numWeeks" :key="`${week}week`">
       <span>{{ week + weekFormat > 52 ? 1 : week + weekFormat }}</span>
     </div>
   </div>
@@ -11,39 +11,40 @@ export default {
   name: 'weeks',
   computed: {
     get() {
-      return this.$store.getters
+      return this.$store.getters;
     },
     weekFormat() {
       return Number(this.get.getFirstWeek) - 1;
     },
   },
-}
+};
 </script>
 
 <style>
-  .weeks {
-    display: grid;
-    grid-gap: .5rem;
-  }
+.weeks {
+  display: grid;
+  grid-gap: 0.5rem;
+}
 
-  .week {
-    font-size: 1rem;
-    padding: 2em 3em;
-    background-color: var(--blue);
-    margin-right: .5rem;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    position: relative;
-  }
+.week {
+  font-size: 1rem;
+  padding: 2em 3em;
+  background-color: #fff;
+  margin-right: 0.5rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  border-radius: 10px;
+}
 
-  .week:nth-last-child() {
-    margin-bottom: 0;
-  }
+.week:nth-last-child() {
+  margin-bottom: 0;
+}
 
-  .week:first-child::before {
-    content: 'Week';
-    position: absolute;
-    top: .5rem;
-  }
+.week:first-child::before {
+  content: 'Week';
+  position: absolute;
+  top: 0.5rem;
+}
 </style>
