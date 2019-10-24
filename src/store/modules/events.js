51ab9cbe5178b3fd71d09c5dd789a62d1c6f1570
Joachim Ekström <joachim.ekstroem@gmail.com>
2019-10-24 11:03:22 +0200

    Merge with master

--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -71,7 +71,6 @@ const mutations = {
   setEvents(commit, data) {
     state.loading = false;
     state.events = data;
-
   },
   findEvent(state, data) {
     state.events = data;
@@ -90,8 +89,8 @@ const mutations = {
   setCurrentValue(state, payload) {
     state.current = payload;
   },
-  loading(state, payload) {
-    return state.loading = true;
+  loading(state) {
+    state.loading = true;
   },
 };
 
